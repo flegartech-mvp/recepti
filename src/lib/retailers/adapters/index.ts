0647d5f70976cc0ef7f7da01e847da342f1b698a
flegartech-mvp <flegartech@gmail.com>
2026-07-18 10:57:09 +0200

    Update recipe app UI and features

--- src/lib/retailers/adapters/index.ts
+++
@@ -1,9 +0,0 @@
-import { hoferSiAdapter } from "./hofer-si";
-import { lidlSiAdapter } from "./lidl-si";
-import { sparSiAdapter } from "./spar-si";
-
-export const retailerAdapters = [
-  sparSiAdapter,
-  hoferSiAdapter,
-  lidlSiAdapter,
-] as const;
